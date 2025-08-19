180d90f97613332650636039185c3557ef786f0b
Rounak <rounakbasak.personal@gmail.com>
2025-08-19 11:20:13 +0530

    frontend done

--- index.php
+++ index.php
@@ -46,7 +46,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
         crossorigin="anonymous"></script>
-    <script src="public/static/404.js"></script>
 </body>
 
 </html>
